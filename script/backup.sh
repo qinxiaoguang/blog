3cbecdb2f859ed8a43ffc265fb0f7e2b6ee37a35
qxg <583462423@qq.com>
2020-05-07 21:50:41 +0800

    upload img

--- script/backup.sh
+++ script/backup.sh
@@ -1,5 +1,6 @@
 output=$1 # 项目执行路径
 cwd=$(cd `dirname $0`;pwd)
+uploadimg=$cwd/../web/static/img/upload
 cd $cwd
 # 第二个参数是mongo的ip,第三个参数是mongo的port
 
@@ -13,6 +14,6 @@ curl "localhost:8080/article/dump"
 rm -rf ./dump/md~
 mv -b $output/md ./dump/
 # 在执行，一定要配置ssh-gen,否则执行失败
-git pull && git add .  && git commit -m "backup db"  && git push origin master
+git pull && git add . && git add $uploadimg  && git commit -m "backup db"  && git push origin master
 
 
